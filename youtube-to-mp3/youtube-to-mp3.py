3fa7a17787332873f9514b01915a26b02f0eece7
Kobee Raveendran <32165720+kobeeraveendran@users.noreply.github.com>
2018-04-06 23:31:45 -0400

    added ease of use/compatibility features
    now removes old mp4 download after successful conversion to mp3
    auto-creates the default directory if it doesn't exist already

--- youtube-to-mp3/youtube-to-mp3.py
+++ youtube-to-mp3/youtube-to-mp3.py
@@ -15,20 +15,24 @@ print("Video title: " + yt.title)
 
 # get the specific stream with audio only (mp4)
 video = yt.streams.get_by_itag(140)
-print("get stream successful")
 
-# my default directory for downloads is D:/Downloads
+# create default directory to store downloaded files in
 # (change this to whatever path suits your needs, i.e. "C:/Users/[name]/Downloads")
-default_directory = "C:/YTDownloads"
+default_directory = "C:/YouTubeDownloads"
+
+if not os.path.exists(default_directory):
+    os.makedirs(default_directory)
+
 video.download(default_directory)
 
-print("video download successful")
-
 # get default filename
-#default_filename = stream[videonum].default_filename
 default_filename = video.default_filename
 mp3_filename = yt.title.replace(" ", "_") + ".mp3"
 
 subprocess.call(['ffmpeg', '-i', os.path.join(default_directory, default_filename), os.path.join(default_directory, mp3_filename)])
 
+# deletes mp4 file after conversion
+os.remove(os.path.join(default_directory, default_filename))
+
+# source code/bug reports
 print("\n\nVideo conversion complete. For source code or issues, visit github.com/kobeeraveendran")
