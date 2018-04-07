ea95ce9a966fe90daea5863c795556790726d330
Kobee Raveendran <32165720+kobeeraveendran@users.noreply.github.com>
2018-04-06 23:13:41 -0400

    fixed issues with ffmpeg file format conversion

--- youtube-to-mp3/youtube-to-mp3.py
+++ youtube-to-mp3/youtube-to-mp3.py
@@ -14,25 +14,30 @@ yt = YouTube(link)
 print("Video title: " + yt.title)
 
 # get all stream formats for the video and display them
-stream = yt.streams.all()
-
-i = 1
-for videos in stream:
-    print(str(i) + ") " + str(videos))
-    i += 1
+#videos = yt.streams.get_by_itag("140")
+video = yt.streams.get_by_itag(140)
+print("get stream successful")
+#i = 1
+#for videos in videos:
+#    print(str(i) + ") " + str(videos))
+#    i += 1
 
 # request stream format from user
-videonum = int(input("Enter video stream number: "))
+#videonum = int(input("Enter video stream number: "))
 
 # my default directory for downloads
-# (change this to whatever path suits your needs)
+# (change this to whatever path suits your needs, i.e. "C:/Downloads")
 default_directory = "D:/Downloads"
-stream[videonum].download(default_directory)
+video.download(default_directory)
+
+print("video download successful")
+#stream[videonum].download(default_directory)
 
 # get default filename
-default_filename = stream[videonum].default_filename
-mp3_filename = yt.title.strip() + ".mp3"
+#default_filename = stream[videonum].default_filename
+default_filename = video.default_filename
+mp3_filename = yt.title.replace(" ", "_") + ".mp3"
 
-subprocess.run(['ffmpeg', '-i', os.path.join(default_directory, default_filename), os.path.join(default_directory, mp3_filename)])
+subprocess.call(['ffmpeg', '-i', os.path.join(default_directory, default_filename), os.path.join(default_directory, mp3_filename)])
 
 print("Video conversion complete. For source code or issues, visit github.com/kobeeraveendran")
