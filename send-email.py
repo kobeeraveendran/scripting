eacd30b187413b5766a0ed2bce8df8f8655ce970
Kobee Raveendran <32165720+kobeeraveendran@users.noreply.github.com>
2018-04-27 22:14:40 -0400

    initial commit
    sends emails (with email body in the message variable) through python

---
+++ send-email.py
@@ -0,0 +1,35 @@
+# Kobee Raveendran
+# sending emails with Python
+
+################################################
+#                                              #
+# NOTE: In order for this to work,             #
+#       enable "Less secure apps"              #
+#       at myaccount.google.com/lessecureapps  #
+#                                              #
+################################################
+
+import smtplib
+# this contains the email address and password; create one and specify
+# these fields yourself to login
+from config import getEmail, getPassword
+
+def send_email(subject, message):
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', port = 587)
+        server.ehlo()
+        server.starttls()
+
+        # login (email and password are in another python file for confidentiality)
+        server.login(getEmail(), getPassword())
+        msg = 'Subject: {} \n\n{}'.format(subject, message)
+        server.sendmail(getEmail(), getEmail(), msg)
+        server.close()
+        print('Message sent successfully')
+    except:
+        print('Email failed to send')
+
+subject = "Initial testing"
+message = "TESTYTEST"
+
+send_email(subject, message)
